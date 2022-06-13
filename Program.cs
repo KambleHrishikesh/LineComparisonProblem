8cb49304e5fdc191d4df4f21ca0059b5eee0f417
hrishikesh kamble <kamblehrishi13@gmail.com>
2022-06-13 19:57:47 +0530

    welcome message

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace LineComparisonProblem
+{
+    public class Program
+    {
+        public static void Main(string[]args)
+        {
+            Console.WriteLine("Welcome to Line Comparison Computation Program");
+        }
+    }
+}
+
+
+
